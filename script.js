602719c6075f9251cf0bbd523a430330aa10f6dd
LORD111051 <nurboyevamunira44@gmail.com>
2024-12-24 15:51:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let ot = prompt("Ismingizni kiriting");
+let space = prompt("Space idingizni kiriting");
+let password = prompt("Space posswordingizni kiriting");
+
+let all = {
+    name: ot,
+    id: space,
+    password: password
+};
+
+console.log(all);
